38f1509ffee1ba8ea49bc4dcc8a00b002fa561ab
NatalyKrotchenkoKPI <nataly.krotchenko@gmail.com>
2024-07-29 18:35:14 +0300

    14 commit

--- cypress.config.js
+++ cypress.config.js
@@ -1,15 +1,16 @@
 const { defineConfig } = require("cypress");
-const cucumber = require("cypress-cucumber-preprocessor").default;
+const cucumber = require("@badeball/cypress-cucumber-preprocessor").default;
+
 module.exports = defineConfig({
   projectId: 'c9zkvb',
- e2e: {
-  baseUrl: "https://conduit.realworld.how",
-   specPattern: "**/*.feature",
-   setupNodeEvents(on, config) {
-     on("file:preprocessor", cucumber());
-   },
- },
- reporter: 'mochawesome',
+  e2e: {
+    baseUrl: "https://conduit.realworld.how",
+    specPattern: "**/*.feature",
+    setupNodeEvents(on, config) {
+      on("file:preprocessor", cucumber());
+    },
+  },
+  reporter: 'mochawesome',
   reporterOptions: {
     reportDir: 'mochawesome-report',
     overwrite: false,
